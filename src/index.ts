337b6679cd79c6999108b77573c567a7bb262130
Bruno Andrade <brunoan99@gmail.com>
2025-12-04 16:52:15 -0300

    feat: complete 'essential - handler'

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { Elysia, t } from "elysia";
+import { Elysia, file, form, t } from "elysia";
 import { fromTypes, openapi } from "@elysiajs/openapi";
 
-// GETTING STARTED - AT GLANCE
 const app = new Elysia()
+
+// GETTING STARTED - AT GLANCE
+const getting_started_at_glance = new Elysia()
   .use(
     openapi({
       references: fromTypes(),
@@ -75,9 +77,57 @@ const user2 = new Elysia({ prefix: "user2 " })
 
 
 // ESSENTIAL - HANDLER
+const essential_handler = new Elysia()
+  // can be alse writen as () => 'Hello Elysia'
+  // A handler can be a literal
+  .get("/handler", "Hello Elysia")
+  // A handler return directly a file
+  .get("/package", file("package.json"))
+  // A handler has a context
+  .get("/context", (context) => context)
+  // And this context object can be destructed to get a desired property directly
+  .get("/status", ({ status }) => status(418, "I'm a teapot"))
+
+  .get("/set", ({ set, status }) => {
+    set.headers = { "X-Teapot": "true" }
+    set.headers["x-powered-by"] = "Elysia"
+
+    return status(418, "I'm a teapot")
+  })
+  .get("/redirect", ({ redirect }) => {
+    return redirect("https://example.com")
+    // or redirect("https://example.com", custom_status) to redirect with custom status
+  })
+  .get("/cookie", ({ cookie: { name } }) => {
+    let cookie_name = name.value;
+    name.value = "New Value"
+    return `Before: ${cookie_name}, After: ${name.value}`
+  })
+  .get("/form", () => form({
+    name: "Form Data",
+    images: []
+  }))
+  .get("/stream", function* () {
+    yield 1
+    yield 2
+    yield 3
+  })
+  .get("/streamok", function* () {
+    if (Math.random() > 0.2) return "ok"
+    yield 1
+    yield 2
+    yield 3
+  })
+  .get('/user-agent', ({ request }) => {
+    return request.headers.get('user-agent')
+  })
 
 // END
-app.use(essential_route).use(user2)
+app
+  .use(getting_started_at_glance)
+  .use(essential_route)
+  .use(essential_handler)
+  .use(user2)
   .listen(3000)
 
 export type App = typeof app;
